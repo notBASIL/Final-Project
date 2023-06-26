1adbe57b3cb8cadd808afb7075537754a1e7002d
Jeril Johnson N <jeril95.n@gmail.com>
2023-06-26 18:48:29 -0400

    Missing safe area provider added which was causing the apk to crash.

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { setStatusBarBackgroundColor, StatusBar } from 'expo-status-bar';
-import { SafeAreaView, Text, View,} from 'react-native';
+import { SafeAreaView, Text, View, } from 'react-native';
 import uuid from 'react-uuid';
 import Form from './src/components/Form/Form';
 import Header from './src/components/Header/Header';
@@ -10,7 +10,7 @@ import About from './src/components/About/about';
 import getPosts from './src/database/read';
 import Filter from './src/components/Filter/Filter';
 import Favourites from './src/components/Favourites/Favourites';
-
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons } from 'react-native-vector-icons';
@@ -26,7 +26,7 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   // read the data from the database and use useEffect to update the data
   const [recipes, setRecipes] = useState([]);
-  
+
   const refresh = () => {
     getPosts(setRecipes);
     console.log('data fetched')
@@ -36,7 +36,7 @@ export default function App() {
     refresh();
   }, []);
 
- 
+
 
 
   // add a new recipe to the database
@@ -63,60 +63,62 @@ export default function App() {
   const favoriteRecipes = recipes.filter((recipe) => recipe.favorite);
 
   return (
-    <NavigationContainer>
-      <StatusBar style="auto" />
-      <SafeAreaView
-      >
-      <Header />
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <StatusBar style="auto" />
+        <SafeAreaView
+        >
+          <Header />
 
-      </SafeAreaView>
+        </SafeAreaView>
 
-      <Tab.Navigator
-        initialRouteName="Recipes" 
-        tabBarActiveTintColor="red"
-      >
+        <Tab.Navigator
+          initialRouteName="Recipes"
+          tabBarActiveTintColor="red"
+        >
 
-        <Tab.Screen name="Recipes" options={{
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="format-list-bulleted" color={color} size={size} />
-          ),
-        }}>
-          {() => <Recipes recipes={recipes} refresh={refresh}/>}
-        </Tab.Screen>
+          <Tab.Screen name="Recipes" options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="format-list-bulleted" color={color} size={size} />
+            ),
+          }}>
+            {() => <Recipes recipes={recipes} refresh={refresh} />}
+          </Tab.Screen>
 
-        <Tab.Screen name="Add Recipe" options={{
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="table-plus" color={color} size={size} />
-          ),
-        }}>
-          {() => <Form onAddRecipe={addRecipe} refresh={refresh}/>}
-        </Tab.Screen>
+          <Tab.Screen name="Add Recipe" options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="table-plus" color={color} size={size} />
+            ),
+          }}>
+            {() => <Form onAddRecipe={addRecipe} refresh={refresh} />}
+          </Tab.Screen>
 
-        <Tab.Screen name="Filter" options={{
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="filter" color={color} size={size} />
-          ),
-        }}>
-           { () => <Filter recipes={recipes} refresh={refresh}/> }
-        </Tab.Screen>
+          <Tab.Screen name="Filter" options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="filter" color={color} size={size} />
+            ),
+          }}>
+            {() => <Filter recipes={recipes} refresh={refresh} />}
+          </Tab.Screen>
 
-        <Tab.Screen name="Favourites" options={{
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="heart" color={color} size={size} />
-         ),  
-        }}>
-          { () => <Favourites recipes={recipes} refresh={refresh}/> }
-        </Tab.Screen>
+          <Tab.Screen name="Favourites" options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="heart" color={color} size={size} />
+            ),
+          }}>
+            {() => <Favourites recipes={recipes} refresh={refresh} />}
+          </Tab.Screen>
 
-        <Tab.Screen name="About" options={{
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="information-outline" color={color} size={size} />
-          ),
-        }}>
-          {() => <About onAddRecipe={addRecipe} refresh={refresh}/>}
-        </Tab.Screen>
+          <Tab.Screen name="About" options={{
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="information-outline" color={color} size={size} />
+            ),
+          }}>
+            {() => <About onAddRecipe={addRecipe} refresh={refresh} />}
+          </Tab.Screen>
 
-      </Tab.Navigator>
-    </NavigationContainer>
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
