ee96b5a148729ec113f19d8a4cd3ca7d93335e52
Jeril Johnson N <jeril95.n@gmail.com>
2023-07-15 03:56:02 -0400

    [#25] Filters grouped into sets of two
    Closes #25

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -14,7 +14,7 @@ export default function Filter(props) {
   useEffect(() => {
     // Filter recipes based on the selected category
     let filteredRecipes = props.recipes.filter(
-      (recipe) => recipe.category === selectedCategory && recipe.cuisine === selectedCuisine 
+      (recipe) => recipe.category === selectedCategory && recipe.cuisine === selectedCuisine
     );
     if (selectedPreparationTime !== "All") {
       filteredRecipes = filteredRecipes.filter(
@@ -94,37 +94,39 @@ export default function Filter(props) {
             </Picker>
           </View>
         </View>
-        <View style={styles.dropdownContainer}>
-          <Text style={styles.dropdownLabel}>Select a Cuisine</Text>
-          <Picker
-            selectedValue={selectedCuisine}
-            onValueChange={handleCuisineChange}
-            style={styles.dropdown}
-          >
-            <Picker.Item label="Chinese" value="Chinese" />
-            <Picker.Item label="Western" value="Western" />
-            <Picker.Item label="Italian" value="Italian" />
-            <Picker.Item label="Indian" value="Indian" />
-          </Picker>
+        <View style={styles.parentContainer}>
+          <View style={styles.dropdownContainer}>
+            <Text style={styles.dropdownLabel}>Select a Cuisine</Text>
+            <Picker
+              selectedValue={selectedCuisine}
+              onValueChange={handleCuisineChange}
+              style={styles.dropdown}
+            >
+              <Picker.Item label="Chinese" value="Chinese" />
+              <Picker.Item label="Western" value="Western" />
+              <Picker.Item label="Italian" value="Italian" />
+              <Picker.Item label="Indian" value="Indian" />
+            </Picker>
 
-        </View>
-        <View style={styles.dropdownContainer}>
-          <Text style={styles.dropdownLabel}>Preparation time</Text>
-          <Picker
-            selectedValue={selectedPreparationTime}
-            onValueChange={handlePreparationTimeChange}
-            style={styles.dropdown}
-          >
-            <Picker.Item label="All" value="All" />
-            <Picker.Item label="0 - 10 minutes" value="0 - 10 minutes" />
-            <Picker.Item label="10 - 15 minutes" value="10 - 15 minutes" />
-            <Picker.Item label="15 - 20 minutes" value="15 - 20 minutes" />
-            <Picker.Item label="20 - 25 minutes" value="20 - 25 minutes" />
-            <Picker.Item label="25 - 30 minutes" value="25 - 30 minutes" />
-            <Picker.Item label="30 - 35 minutes" value="30 - 35 minutes" />
-            <Picker.Item label="35 - 40 minutes" value="35 - 40 minutes" />
-            <Picker.Item label="45+ minutes" value="45+ minutes" />
-          </Picker>
+          </View>
+          <View style={styles.dropdownContainer}>
+            <Text style={styles.dropdownLabel}>Preparation time</Text>
+            <Picker
+              selectedValue={selectedPreparationTime}
+              onValueChange={handlePreparationTimeChange}
+              style={styles.dropdown}
+            >
+              <Picker.Item label="All" value="All" />
+              <Picker.Item label="0 - 10 minutes" value="0 - 10 minutes" />
+              <Picker.Item label="10 - 15 minutes" value="10 - 15 minutes" />
+              <Picker.Item label="15 - 20 minutes" value="15 - 20 minutes" />
+              <Picker.Item label="20 - 25 minutes" value="20 - 25 minutes" />
+              <Picker.Item label="25 - 30 minutes" value="25 - 30 minutes" />
+              <Picker.Item label="30 - 35 minutes" value="30 - 35 minutes" />
+              <Picker.Item label="35 - 40 minutes" value="35 - 40 minutes" />
+              <Picker.Item label="45+ minutes" value="45+ minutes" />
+            </Picker>
+          </View>
         </View>
         {
           // map through the recipe and display them or else display a message
--- src/components/Filter/styles.js
+++ src/components/Filter/styles.js
@@ -1,14 +1,18 @@
 import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
-    container: {
-        alignSelf: 'stretch',
-        flex: 1,
-        backgroundColor: '#f0efed',
-      },
-      dropdownLabel: {
-        fontSize: 16,
-        fontWeight: 'bold',
-        color: 'red'
-    }
+  parentContainer: {
+    flexDirection: 'row', // Added to display the dropdowns side by side
+    justifyContent: 'space-between', // Added to evenly distribute the dropdowns
+    marginBottom: 20, // Added to provide spacing between parent containers
+  },
+  dropdownContainer: {
+    flex: 1, // Added to evenly distribute the dropdowns within the parent container
+    marginRight: 10, // Added to provide spacing between dropdown containers
+  },
+  dropdownLabel: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'red',
+  }
 });
 export default styles;
