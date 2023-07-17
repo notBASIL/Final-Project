17e30e513f01ff552b698fd63cebbbdb9999abcd
noelabraham97 <n_abrahambiju@fanshaweonline.ca>
2023-07-16 23:06:59 -0400

    [#23]implemented sorting of saved recipes alphabetically. A button is created for sorting the recipes in ascending order or descending order alphabetically.
    Closed #23

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -4,86 +4,111 @@ import Recipe from "./Recipe/Recipe";
 import styles from "./styles";
 
 export default function Recipes(props) {
-    const [recipes, setRecipes] = useState([]);
-    const [searchQuery, setSearchQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc");
 
-    useEffect(() => {
-      if (searchQuery) {
-        const filteredRecipes = props.recipes.filter((recipe) => {
-          const recipeName = recipe.name ? recipe.name.toLowerCase() : '';
-          const cuisine = recipe.cuisine ? recipe.cuisine.toLowerCase() : '';
-          const ingredients = [
-            recipe.ingredient1,
-            recipe.ingredient2,
-            recipe.ingredient3,
-          ]
-            .filter((ingredient) => ingredient) // Filter out undefined ingredients
-            .join(' ')
-            .toLowerCase();
-    
-          return (
-            recipeName.includes(searchQuery.toLowerCase()) ||
-            cuisine.includes(searchQuery.toLowerCase()) ||
-            ingredients.includes(searchQuery.toLowerCase())
-          );
-        });
-    
-        setRecipes(filteredRecipes);
+  useEffect(() => {
+    if (searchQuery) {
+      const filteredRecipes = props.recipes.filter((recipe) => {
+        const recipeName = recipe.name ? recipe.name.toLowerCase() : "";
+        const cuisine = recipe.cuisine ? recipe.cuisine.toLowerCase() : "";
+        const ingredients = [
+          recipe.ingredient1,
+          recipe.ingredient2,
+          recipe.ingredient3,
+        ]
+          .filter((ingredient) => ingredient)
+          .join(" ")
+          .toLowerCase();
+
+        return (
+          recipeName.includes(searchQuery.toLowerCase()) ||
+          cuisine.includes(searchQuery.toLowerCase()) ||
+          ingredients.includes(searchQuery.toLowerCase())
+        );
+      });
+
+      setRecipes(filteredRecipes);
+    } else {
+      setRecipes(props.recipes);
+    }
+  }, [props.recipes, searchQuery]);
+
+  useEffect(() => {
+    sortRecipes();
+  }, [sortOrder]);
+
+  const handleStatusChange = (recipeId, newStatus) => {
+    setRecipes((prevRecipes) =>
+      prevRecipes.map((recipe) =>
+        recipe.id === recipeId ? { ...recipe, favorite: newStatus } : recipe
+      )
+    );
+  };
+
+  const handleDelete = (recipeId) => {
+    setRecipes((prevRecipes) =>
+      prevRecipes.filter((recipe) => recipe.id !== recipeId)
+    );
+  };
+
+  const favoriteRecipes = recipes.filter((recipe) => recipe.done);
+
+  const sortRecipes = () => {
+    const sortedRecipes = [...recipes].sort((a, b) => {
+      const recipeNameA = a.name.toLowerCase();
+      const recipeNameB = b.name.toLowerCase();
+      if (sortOrder === "asc") {
+        return recipeNameA.localeCompare(recipeNameB);
       } else {
-        setRecipes(props.recipes);
+        return recipeNameB.localeCompare(recipeNameA);
       }
-    }, [props.recipes, searchQuery]);
-    
-    
+    });
+    setRecipes(sortedRecipes);
+  };
 
-    const handleStatusChange = (recipeId, newStatus) => {
-      setRecipes((prevRecipes) =>
-        prevRecipes.map((recipe) =>
-          recipe.id === recipeId ? { ...recipe, favorite: newStatus } : recipe
-        )
-      );
-    };
-    
-    const handleDelete = (recipeId) => {
-      setRecipes((prevRecipes) =>
-        prevRecipes.filter((recipe) => recipe.id !== recipeId)
-      );
-    };
-  
-    const favoriteRecipes = recipes.filter((recipe) => recipe.done);
+  const handleSortOrderChange = () => {
+    setSortOrder((prevSortOrder) => (prevSortOrder === "asc" ? "desc" : "asc"));
+  };
 
-    
-    
   return (
     <View style={styles.container}>
-        <TextInput
-          placeholder="Search recipes..."
-          value={searchQuery}
-          onChangeText={setSearchQuery}
-          style={styles.searchInput}
-        />
+      <Button
+        title={sortOrder === "asc" ? "Sort A-Z" : "Sort Z-A"}
+        onPress={handleSortOrderChange}
+        style={styles.button}
+      />
+      <TextInput
+        placeholder="Search recipes..."
+        value={searchQuery}
+        onChangeText={setSearchQuery}
+        style={styles.searchInput}
+      />
       <ScrollView>
-        {
-            // map through the recipe and display them or else display a message
-            recipes.length > 0 ? recipes.map((recipe) => (
-              <Recipe
-                key={recipe.id}
-                recipe={recipe}
-                onStatusChange={handleStatusChange}
-                onDelete={props.onDelete}
-                refresh={props.refresh}
-                showToggleSwitch={true}
-              />
-            )) : <Text style={{
-                textAlign: 'center',
-                fontSize: 15,
-                marginTop: 250
-
-            }}>You don't have any Recipes</Text>
-        }
-
-              </ScrollView>
-      
+        {recipes.length > 0 ? (
+          recipes.map((recipe) => (
+            <Recipe
+              key={recipe.id}
+              recipe={recipe}
+              onStatusChange={handleStatusChange}
+              onDelete={props.onDelete}
+              refresh={props.refresh}
+              showToggleSwitch={true}
+            />
+          ))
+        ) : (
+          <Text
+            style={{
+              textAlign: "center",
+              fontSize: 15,
+              marginTop: 250,
+            }}
+          >
+            You don't have any Recipes
+          </Text>
+        )}
+      </ScrollView>
     </View>
   );
 }
