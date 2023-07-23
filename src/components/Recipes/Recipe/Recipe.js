86542dc3f2176cd0070f4c1d84fb5c235019b990
Jeril Johnson N <jeril95.n@gmail.com>
2023-07-23 06:59:22 -0400

    Logic updated for conditional rendering of recipe fields

--- src/components/Recipes/Recipe/Recipe.js
+++ src/components/Recipes/Recipe/Recipe.js
@@ -6,7 +6,7 @@ import deleteRecipe from "../../../database/delete";
 import updateRecipe from "../../../database/update";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { TouchableOpacity } from "react-native";
-import { MaterialIcons } from "@expo/vector-icons"; 
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function Recipe(props) {
   const [modalVisible, setModalVisible] = useState(false);
@@ -93,60 +93,6 @@ export default function Recipe(props) {
               </TouchableOpacity>
             )}
           </View>
-          {/* <View>
-            <Text style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "center",
-              paddingTop: 10,
-              color: "#870F4F",
-              fontWeight: "bold",
-              fontSize: 15,
-              }} >Ingredients</ Text>
-          </View>
-          <View style={styles.ingredientContainer}>
-            <Text style={styles.ingredientText}>
-              {props.recipe?.ingredient1}
-            </Text>
-          </View>
-
-          {props.recipe?.ingredient2 && (
-            <View style={styles.ingredientContainer}>
-              <Text style={styles.ingredientText}>
-                {props.recipe.ingredient2}
-              </Text>
-            </View>
-          )}
-
-          {props.recipe?.ingredient3 && (
-            <View style={styles.ingredientContainer}>
-              <Text style={styles.ingredientText}>
-                {props.recipe.ingredient3}
-              </Text>
-            </View>
-          )} */}
-          {/* <View>
-            <Text style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "center",
-              paddingTop: 10,
-              color: "#870F4F",
-              fontWeight: "bold",
-              fontSize: 15,
-              }} >Instructions</ Text>
-          </View>
-          
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "center",
-              paddingTop: 10,
-            }}
-          >
-            <Text>{props.recipe.instructions}</Text>
-          </View> */}
           <Text
             style={{
 
@@ -192,26 +138,6 @@ export default function Recipe(props) {
           {!props.recipe.glutenFree && props.recipe.lactoseFree && (
             <Text style={styles.label}>Lactose Free</Text>
           )}
-
-          {/* {props.showToggleSwitch && (
-            <View style={styles.switch}>
-              <Text style={styles.switchText}>Favourite</Text>
-              <Switch value={favourite} onValueChange={handleFavouriteChange} />
-            </View>
-          )} */}
-
-          {/* <Text
-          style={{
-            padding: 5,
-            borderRadius: 5,
-            fontSize: 15,
-            fontWeight: "bold",
-            width: "100%",
-            marginTop: 5,
-            marginLeft: 242,
-            color: "green",
-          }}
-          >Favourite:</Text> */}
           <Text style={{
 
             fontSize: 15,
@@ -224,20 +150,20 @@ export default function Recipe(props) {
       </TouchableOpacity>
       <Modal visible={modalVisible}>
         <View style={styles.modalView}>
-        <View style={styles.modalHeader}>
-      <TouchableOpacity onPress={handleModalVisible} style={styles.backIcon}>
-        <MaterialIcons name="keyboard-arrow-left" size={24} color="black" />
-      </TouchableOpacity>
-      <Text style={styles.headerTitle}>{props.recipe.name}</Text>
-      <TouchableOpacity onPress={handleDelete} style={styles.deleteIcon}>
-        <MaterialIcons name="delete" size={24} color="black" />
-      </TouchableOpacity>
-    </View>
-    <View style={styles.switch}>
-      <Text style={styles.unitText}>g</Text>
-      <Switch value={isMetric} onValueChange={() => setIsMetric(!isMetric)} />
-      <Text style={styles.unitText}>oz</Text>
-    </View>
+          <View style={styles.modalHeader}>
+            <TouchableOpacity onPress={handleModalVisible} style={styles.backIcon}>
+              <MaterialIcons name="keyboard-arrow-left" size={24} color="black" />
+            </TouchableOpacity>
+            <Text style={styles.headerTitle}>{props.recipe.name}</Text>
+            <TouchableOpacity onPress={handleDelete} style={styles.deleteIcon}>
+              <MaterialIcons name="delete" size={24} color="black" />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.switch}>
+            <Text style={styles.unitText}>g</Text>
+            <Switch value={isMetric} onValueChange={() => setIsMetric(!isMetric)} />
+            <Text style={styles.unitText}>oz</Text>
+          </View>
           <View style={styles.alignContainer}>
             <Pressable onPress={handleIngredientPress1}>
               <View
@@ -251,7 +177,7 @@ export default function Recipe(props) {
                 </Text>
               </View>
             </Pressable>
-            {props.recipe.quantity1 && (
+            {props.recipe.quantity1 !== 0.0 && (
               // Quantity 1 with the switch
               <View style={styles.quantityContainer}>
                 <Text style={styles.ingredientText}>
@@ -263,7 +189,7 @@ export default function Recipe(props) {
             )}
           </View>
           <View style={styles.alignContainer}>
-            {props.recipe.ingredient2 && (
+            {props.recipe.ingredient2 !== "" && (
               <Pressable onPress={handleIngredientPress2}>
                 <View
                   style={[
@@ -277,7 +203,7 @@ export default function Recipe(props) {
                 </View>
               </Pressable>
             )}
-            {props.recipe.quantity2 && (
+            {props.recipe.quantity2 !== 0.0 && (
               // Quantity 1 with the switch
               <View style={styles.quantityContainer}>
                 <Text style={styles.ingredientText}>
@@ -289,7 +215,7 @@ export default function Recipe(props) {
             )}
           </View>
           <View style={styles.alignContainer}>
-            {props.recipe.ingredient3 && (
+            {props.recipe.ingredient3 !== "" && (
               <Pressable onPress={handleIngredientPress3}>
                 <View
                   style={[
@@ -303,7 +229,7 @@ export default function Recipe(props) {
                 </View>
               </Pressable>
             )}
-            {props.recipe.quantity3 && (
+            {props.recipe.quantity3 !== 0.0 && (
               // Quantity 1 with the switch
               <View style={styles.quantityContainer}>
                 <Text style={styles.ingredientText}>
@@ -350,33 +276,6 @@ export default function Recipe(props) {
             {!props.recipe.glutenFree && props.recipe.lactoseFree && (
               <Text style={styles.label2}>Lactose Free</Text>
             )}
-
-            {/* <Pressable onPress={handleDelete}>
-              <Text style={{
-                backgroundColor: "red",
-                padding: 10,
-                borderRadius: 10,
-                fontSize: 15,
-                fontWeight: "bold",
-                marginTop: 1,
-                marginBottom: 10,
-                textAlign: "center",
-                color: "white"
-              }}>Delete</Text>
-            </Pressable>
-            <Pressable onPress={handleModalVisible}>
-              <Text style={{
-                backgroundColor: "green",
-                padding: 10,
-                borderRadius: 10,
-                fontSize: 15,
-                fontWeight: "bold",
-                marginTop: 1,
-                marginBottom: 10,
-                textAlign: "center",
-                color: "white"
-              }}>Close</Text>
-            </Pressable> */}
           </View>
         </View>
       </Modal>
